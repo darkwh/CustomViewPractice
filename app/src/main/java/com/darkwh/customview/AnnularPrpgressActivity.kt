42c8810cd82b1c18974f26eca74a341e0dba177a
wanghuan <wanghuan@izaodao.org>
2017-12-11 17:37:27 +0800

    文字绘制有一处问题(当前进度)

--- app/src/main/java/com/darkwh/customview/AnnularPrpgressActivity.kt
+++ app/src/main/java/com/darkwh/customview/AnnularPrpgressActivity.kt
@@ -3,9 +3,10 @@ package com.darkwh.customview
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class AnnularPrpgressActivity : AppCompatActivity(){
+class AnnularPrpgressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_annular)
     }
 }
