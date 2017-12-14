eaf3471f8a657d17c927a1f5655afab5af7b4ff8
wanghuan <wanghuan@izaodao.org>
2017-12-14 18:27:24 +0800

    基本功能完成,动画细节需要继续调整

--- app/src/main/java/com/darkwh/customview/AnnularPrpgressActivity.kt
+++ app/src/main/java/com/darkwh/customview/AnnularPrpgressActivity.kt
@@ -2,11 +2,16 @@ package com.darkwh.customview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_annular.*
 
 class AnnularPrpgressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_annular)
+        annular_bar.setOnClickListener {
+//            annular_bar.playAnimation()
+            annular_bar.setProgressWithAnimation(80)
+        }
     }
 }
